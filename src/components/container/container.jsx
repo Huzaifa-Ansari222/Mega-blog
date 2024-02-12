daec06e78a467896914977979ed6e9bb98a33ac3
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-13 00:06:12 +0530

    add file

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const container = ({children}) => {
   return (
-    <div>container</div>
+    <div className='bg-red-500 w-full max-w-7xl mx-auto px-4 '>{children}</div>
   )
 }
